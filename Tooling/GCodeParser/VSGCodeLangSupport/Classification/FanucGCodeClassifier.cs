fef5a2bda14d6bad08df85927dd1b78b5f4d0c4c
Phil McCarthy <phil_mccarthy24@hotmail.com>
2018-04-22 14:23:53 +0100

    Switched to using built-in classification types

--- Tooling/GCodeParser/VSGCodeLangSupport/Classification/FanucGCodeClassifier.cs
+++ Tooling/GCodeParser/VSGCodeLangSupport/Classification/FanucGCodeClassifier.cs
@@ -8,7 +8,9 @@
     using Microsoft.VisualStudio.Text.Editor;
     using Microsoft.VisualStudio.Text.Tagging;
     using Microsoft.VisualStudio.Utilities;
+    using Microsoft.VisualStudio.Language.StandardClassification;
     using GCodeParser;
+    using System.Diagnostics;
 
     [Export(typeof(ITaggerProvider))]
     [ContentType("FanucGCode")]
@@ -52,49 +54,62 @@
         /// Construct the classifier and define search tokens
         /// </summary>
         internal FanucGCodeClassifier(ITextBuffer buffer, 
-                               ITagAggregator<FanucGCodeTokenTag> ookTagAggregator, 
+                               ITagAggregator<FanucGCodeTokenTag> fanucGCodeTagAggregator, 
                                IClassificationTypeRegistryService typeService)
         {
+            // I found an example of how to use builtin classifications here:
+            // https://github.com/ponylang/VS-pony/blob/master/PonyLanguage/SyntaxHighlighter.cs
+
+            // standard theme mapping:
+            // PredefinedClassificationTypeNames.Comment is green
+            // PredefinedClassificationTypeNames.Keyword is blue
+            // PredefinedClassificationTypeNames.SymbolDefinition is green-blue (like C++/C# custom type)
+            // PredefinedClassificationTypeNames.SymbolReference is bold dark green
+            // PredefinedClassificationTypeNames.PreprocessorKeyword is light grey
+            // PredefinedClassificationTypeNames.Identifier is uncoloured
+            // PredefinedClassificationTypeNames.Operator is uncoloured
+            // PredefinedClassificationTypeNames.String is red
+
             _buffer = buffer;
-            _aggregator = ookTagAggregator;
+            _aggregator = fanucGCodeTagAggregator;
             _fanucGCodeTypes = new Dictionary<FanucGCodeTokenTypes, IClassificationType>();
             // note several different token types will map to the same classification type, eg the scanner will
             // pick out components of a comment including brackets, but we want the whole thing to be classified
             // as a comment editor format.
-            _fanucGCodeTypes[FanucGCodeTokenTypes.CommentStart] = typeService.GetClassificationType("GCodeComment");
-            _fanucGCodeTypes[FanucGCodeTokenTypes.CommentText] = typeService.GetClassificationType("GCodeComment");
-            _fanucGCodeTypes[FanucGCodeTokenTypes.CommentEnd] = typeService.GetClassificationType("GCodeComment");
+            _fanucGCodeTypes[FanucGCodeTokenTypes.CommentStart] = typeService.GetClassificationType(PredefinedClassificationTypeNames.Comment);
+            _fanucGCodeTypes[FanucGCodeTokenTypes.CommentText] = typeService.GetClassificationType(PredefinedClassificationTypeNames.Comment);
+            _fanucGCodeTypes[FanucGCodeTokenTypes.CommentEnd] = typeService.GetClassificationType(PredefinedClassificationTypeNames.Comment);
 
             // functions - should possibly have their own format
-            _fanucGCodeTypes[FanucGCodeTokenTypes.BuiltinFunction] = typeService.GetClassificationType("GCodeKeyword");
-            _fanucGCodeTypes[FanucGCodeTokenTypes.AxNum_Function] = typeService.GetClassificationType("GCodeKeyword");
-            _fanucGCodeTypes[FanucGCodeTokenTypes.Ax_Function] = typeService.GetClassificationType("GCodeKeyword");
-            _fanucGCodeTypes[FanucGCodeTokenTypes.SetVN_Function] = typeService.GetClassificationType("GCodeKeyword");
-            _fanucGCodeTypes[FanucGCodeTokenTypes.BPrnt_Function] = typeService.GetClassificationType("GCodeKeyword");
-            _fanucGCodeTypes[FanucGCodeTokenTypes.DPrnt_Function] = typeService.GetClassificationType("GCodeKeyword");
-            _fanucGCodeTypes[FanucGCodeTokenTypes.POpen_Function] = typeService.GetClassificationType("GCodeKeyword");
-            _fanucGCodeTypes[FanucGCodeTokenTypes.PClos_Function] = typeService.GetClassificationType("GCodeKeyword");
+            _fanucGCodeTypes[FanucGCodeTokenTypes.BuiltinFunction] = typeService.GetClassificationType(PredefinedClassificationTypeNames.SymbolDefinition);
+            _fanucGCodeTypes[FanucGCodeTokenTypes.AxNum_Function] = typeService.GetClassificationType(PredefinedClassificationTypeNames.SymbolDefinition);
+            _fanucGCodeTypes[FanucGCodeTokenTypes.Ax_Function] = typeService.GetClassificationType(PredefinedClassificationTypeNames.SymbolDefinition);
+            _fanucGCodeTypes[FanucGCodeTokenTypes.SetVN_Function] = typeService.GetClassificationType(PredefinedClassificationTypeNames.SymbolDefinition);
+            _fanucGCodeTypes[FanucGCodeTokenTypes.BPrnt_Function] = typeService.GetClassificationType(PredefinedClassificationTypeNames.SymbolDefinition);
+            _fanucGCodeTypes[FanucGCodeTokenTypes.DPrnt_Function] = typeService.GetClassificationType(PredefinedClassificationTypeNames.SymbolDefinition);
+            _fanucGCodeTypes[FanucGCodeTokenTypes.POpen_Function] = typeService.GetClassificationType(PredefinedClassificationTypeNames.SymbolDefinition);
+            _fanucGCodeTypes[FanucGCodeTokenTypes.PClos_Function] = typeService.GetClassificationType(PredefinedClassificationTypeNames.SymbolDefinition);
 
             // keywords
-            _fanucGCodeTypes[FanucGCodeTokenTypes.If] = typeService.GetClassificationType("GCodeKeyword");
-            _fanucGCodeTypes[FanucGCodeTokenTypes.Then] = typeService.GetClassificationType("GCodeKeyword");
-            _fanucGCodeTypes[FanucGCodeTokenTypes.Goto] = typeService.GetClassificationType("GCodeKeyword");
-            _fanucGCodeTypes[FanucGCodeTokenTypes.While] = typeService.GetClassificationType("GCodeKeyword");
-            _fanucGCodeTypes[FanucGCodeTokenTypes.Do] = typeService.GetClassificationType("GCodeKeyword");
-            _fanucGCodeTypes[FanucGCodeTokenTypes.End] = typeService.GetClassificationType("GCodeKeyword");
+            _fanucGCodeTypes[FanucGCodeTokenTypes.If] = typeService.GetClassificationType(PredefinedClassificationTypeNames.Keyword);
+            _fanucGCodeTypes[FanucGCodeTokenTypes.Then] = typeService.GetClassificationType(PredefinedClassificationTypeNames.Keyword);
+            _fanucGCodeTypes[FanucGCodeTokenTypes.Goto] = typeService.GetClassificationType(PredefinedClassificationTypeNames.Keyword);
+            _fanucGCodeTypes[FanucGCodeTokenTypes.While] = typeService.GetClassificationType(PredefinedClassificationTypeNames.Keyword);
+            _fanucGCodeTypes[FanucGCodeTokenTypes.Do] = typeService.GetClassificationType(PredefinedClassificationTypeNames.Keyword);
+            _fanucGCodeTypes[FanucGCodeTokenTypes.End] = typeService.GetClassificationType(PredefinedClassificationTypeNames.Keyword);
 
             // relational and logical operators, and modulus operator which has text rather than symbol
-            _fanucGCodeTypes[FanucGCodeTokenTypes.RelationalOperator] = typeService.GetClassificationType("GCodeKeyword");
-            _fanucGCodeTypes[FanucGCodeTokenTypes.LogicalOperator] = typeService.GetClassificationType("GCodeKeyword");
-            _fanucGCodeTypes[FanucGCodeTokenTypes.Modulus] = typeService.GetClassificationType("GCodeKeyword");
+            _fanucGCodeTypes[FanucGCodeTokenTypes.RelationalOperator] = typeService.GetClassificationType(PredefinedClassificationTypeNames.Keyword);
+            _fanucGCodeTypes[FanucGCodeTokenTypes.LogicalOperator] = typeService.GetClassificationType(PredefinedClassificationTypeNames.Keyword);
+            _fanucGCodeTypes[FanucGCodeTokenTypes.Modulus] = typeService.GetClassificationType(PredefinedClassificationTypeNames.Keyword);
 
             // system vars, constants and common vars should have their text coloured like C# string
-            _fanucGCodeTypes[FanucGCodeTokenTypes.NamedVariable] = typeService.GetClassificationType("GCodeKeyword");
+            _fanucGCodeTypes[FanucGCodeTokenTypes.NamedVariable] = typeService.GetClassificationType(PredefinedClassificationTypeNames.String);
 
             // GCode prefixes need to stand out in the code
-            _fanucGCodeTypes[FanucGCodeTokenTypes.ProgramNumberPrefix] = typeService.GetClassificationType("GCodeKeyword");
-            _fanucGCodeTypes[FanucGCodeTokenTypes.LabelPrefix] = typeService.GetClassificationType("GCodeKeyword");
-            _fanucGCodeTypes[FanucGCodeTokenTypes.GCodePrefix] = typeService.GetClassificationType("GCodeKeyword");
+            _fanucGCodeTypes[FanucGCodeTokenTypes.ProgramNumberPrefix] = typeService.GetClassificationType(PredefinedClassificationTypeNames.SymbolDefinition);
+            _fanucGCodeTypes[FanucGCodeTokenTypes.LabelPrefix] = typeService.GetClassificationType(PredefinedClassificationTypeNames.String);
+            _fanucGCodeTypes[FanucGCodeTokenTypes.GCodePrefix] = typeService.GetClassificationType(PredefinedClassificationTypeNames.SymbolDefinition);
         }
 
         public event EventHandler<SnapshotSpanEventArgs> TagsChanged
@@ -115,6 +130,53 @@
                     new TagSpan<ClassificationTag>(tagSpans[0], 
                                                    new ClassificationTag(_fanucGCodeTypes[tagSpan.Tag.type]));
             }
+        }
+    }
+
+    // Error provider for putting red squiggles under tags that are incorrect
+    [Export(typeof(ITaggerProvider))]
+    [ContentType("FanucGCode")]
+    [TagType(typeof(ErrorTag))]
+    class FanucGCodeErrorProvider : ITaggerProvider
+    {
+        public ITagger<T> CreateTagger<T>(ITextBuffer buffer) where T : ITag
+        {
+            if (buffer == null)
+            {
+                throw new ArgumentException("Buffer is null");
+            }
+            else
+            {
+                return new FanucGCodeErrorTagger() as ITagger<T>;
+            }
+        }
+    }
+
+
+    class FanucGCodeErrorTagger : ITagger<IErrorTag>
+    {
+        private const string _searchText = "]";
+        public IEnumerable<ITagSpan<IErrorTag>> GetTags(NormalizedSnapshotSpanCollection spans)
+        {
+            foreach (SnapshotSpan currSpan in spans)
+            {
+                int loc = currSpan.GetText().ToLower().IndexOf(_searchText);
+
+                if (loc > -1)
+                {
+                    SnapshotSpan CheckTextSpan = new SnapshotSpan(currSpan.Snapshot, new Span(currSpan.Start + loc, _searchText.Length));
+                    // -- this didn't work. how to output an intellisense error??
+                    // Console.WriteLine(@"C:\Users\Phil_\Desktop\GemstoneExamplesSource\ExpectedGCode\Fanuc\O9453_verbose.prg(17,20): error PS0168: The variable 'foo' is declared but never used");
+                    yield return new TagSpan<ErrorTag>(CheckTextSpan, new ErrorTag("Syntax error", "Bwaaaaah!")); // tried "Compilation error", but lose red error positional adornment on right text window margin
+                    // the positional adornment also seems to only be displayed for the first error, and isn't displayed for all the others (however red squiggles are shown for all ']' chars).
+                }
+            }
+        }
+
+        public event EventHandler<SnapshotSpanEventArgs> TagsChanged
+        {
+            add { }
+            remove { }
         }
     }
 }
